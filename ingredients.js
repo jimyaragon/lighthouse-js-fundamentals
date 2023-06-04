2dad148c1326dacfaa883add5ba914a08982fbbc
jimyaragon <jimy.aragon@gmail.com>
2023-06-03 19:40:21 -0600

    Ingredients

---
+++ ingredients.js
@@ -0,0 +1,22 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+console.log("Contents of ingredients:");
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+// Write a for loop that prints out the contents of ingredients:
+console.log("contes of ingredients:")
+for (let i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+console.log("Contents of ingredients:");
+let i = 7;
+while (i < ingredients.length && i > -1) {
+  console.log(ingredients[i]);
+  i--;
+}
